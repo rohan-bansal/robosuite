825452a0a3125a779e9d89fdde3373522772262f
Rohan Bansal <bansalrohan18@gmail.com>
2024-12-27 00:20:45 -0800

    document motion_profile.py

--- robosuite/utils/motion_profile.py
+++ robosuite/utils/motion_profile.py
@@ -3,6 +3,14 @@ import numpy as np
 
 class MotionProfile:
     def __init__(self, default_control_freq=20, intervals=None, integers_only=False):
+        """
+        Initialize the MotionProfile class.
+        
+        :param default_control_freq: Default control frequency.
+        :param intervals: List of intervals with start, end, control frequency, and easing.
+        :param integers_only: Whether to round control frequency to integers.
+        """
+
         self.default_control_freq = default_control_freq
         self.intervals = intervals if intervals is not None else []
         self.integers_only = integers_only
@@ -14,10 +22,11 @@ class MotionProfile:
         :param start_percent: Start percentage of the interval.
         :param end_percent: End percentage of the interval.
         :param control_freq: Control frequency at the start of the interval.
-        :param end_control_freq: Control frequency at the end of the interval (for interpolation).
-        :param easing: Easing function to use for interpolation. Options are:
+        :param end_control_freq: (optional) Control frequency at the end of the interval for interpolation. Will use easing value.
+        :param easing: (optional) Easing function to use for interpolation. Options are:
                       'linear' (default), 'ease_in', 'ease_out', 'ease_in_out'
         """
+
         self.intervals.append((start_percent, end_percent, control_freq, end_control_freq, easing))
         self.intervals.sort(key=lambda x: x[0])
 
@@ -34,6 +43,14 @@ class MotionProfile:
             raise ValueError(f"Unknown easing function: {easing}")
 
     def get_control_frequency(self, current_step, total_steps):
+        """
+        Get the control frequency at the current step.
+
+        :param current_step: Current step number.
+        :param total_steps: Total number of steps.
+        :return: Control frequency at the current step.
+        """
+
         current_percent = (current_step / total_steps) * 100
 
         for start, end, start_freq, end_freq, easing in self.intervals:
@@ -49,7 +66,7 @@ class MotionProfile:
 
     def generate_graph(self, total_steps):
         """
-        Generates a graph of the control frequency over the given number of steps.
+        Utility function to generate a graph of the control frequency over the given number of steps.
         
         :param total_steps: Total number of steps to simulate.
         :return: A matplotlib figure object.
